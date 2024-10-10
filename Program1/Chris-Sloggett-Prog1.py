442e3a16a186bc151876e06f469e9000f9658f2f
Christopher Sloggett <mrsloggett@gmail.com>
2024-10-09 17:29:05 -0700

    Figured out why no solution was found, odd number of inversions with A* doesnt work. Started adding a menu and more functionality

--- Program1/Chris-Sloggett-Prog1.py
+++ Program1/Chris-Sloggett-Prog1.py
@@ -29,6 +29,14 @@ class PuzzleState:
         return self.cost < other.cost
     
 def heuristic1(board):
+    misplaced = 0;
+    for i in range(3):
+        for j in range(3):
+            if board[i][j] != 0 and board[i][j] != 3 * i + j + 1:
+                misplaced += 1
+    return misplaced
+
+def heuristic2(board):
     distance = 0
     for i in range(9):
         if board[i] != 0:
@@ -36,6 +44,9 @@ def heuristic1(board):
             x2, y2 = divmod(board[i] - 1, 3)
             distance += abs(x1 - x2) + abs(y1 - y2)
     return distance
+
+def heuristic3():
+    pass
 
 def move_tile(board, move, blank_pos):
     new_board = board[:]
@@ -68,11 +79,13 @@ def print_solution(solution):
         print(f"Move: {step.move}")
         print_board(step.board)
 
+def best_first_search():
+    pass
 
-def a_star(start_state):
+def a_star(start_state, heuristic):
     open_list = []
     closed_list = set()
-    heapq.heappush(open_list, PuzzleState(start_state, None, None, 0, heuristic1(start_state)))
+    heapq.heappush(open_list, PuzzleState(start_state, None, None, 0, heuristic(start_state)))
 
     while open_list:
         current_state = heapq.heappop(open_list)
@@ -99,15 +112,99 @@ def a_star(start_state):
             if tuple(new_board) in closed_list:
                 continue
 
-            new_state = PuzzleState(new_board, current_state, move, current_state.depth + 1, current_state.depth + 1 + heuristic1(new_board))
+            new_state = PuzzleState(new_board, current_state, move, current_state.depth + 1, current_state.depth + 1 + heuristic(new_board))
             heapq.heappush(open_list, new_state)
 
     return None
 
-solution = a_star(initial_state)
 
-if solution:
-    print(colored("Sloution found: ", "green"))
-    print_solution(solution)
-else:
-    print(colored("No solution found", "red"))
+def main():
+    while True:
+        print("Welcome to the 8 Puzzle using the A* and Best First Search Algorithms!")
+        print("Please choose from the following options:")
+        print("1. A* Algorithm")
+        print("2. Best First Search Algorithm")
+        print("3. Exit")
+
+        choice = input("Enter your choice: ")
+
+        if choice == "1":
+            print("You chose the A* Algorithm")
+            print("The initial state is:")
+            print_board(initial_state)
+            print("Choose a heuristic:")
+            print("1. Misplaced Tiles")
+            print("2. Manhattan Distance")
+            print("3. Unchosen Heuristic")
+
+            a_star_choice = input("Enter your choice: ")
+            
+            if a_star_choice == "1":
+                print("You chose the Misplaced Tiles heuristic")
+                solution = a_star(initial_state, heuristic1)
+                if solution:
+                    print(colored("Sloution found: ", "green"))
+                    print_solution(solution)
+                else:
+                    print(colored("No solution found", "red"))
+            elif a_star_choice == "2":
+                print("You chose the Manhattan Distance heuristic")
+                solution = a_star(initial_state, heuristic2)
+                if solution:
+                    print(colored("Sloution found: ", "green"))
+                    print_solution(solution)
+                else:
+                    print(colored("No solution found", "red"))
+            elif a_star_choice == "3":
+                print("You chose the Unchosen Heuristic")
+                solution = a_star(initial_state, heuristic1)
+                if solution:
+                    print(colored("Sloution found: ", "green"))
+                    print_solution(solution)
+                else:
+                    print(colored("No solution found", "red"))
+
+        elif choice == "2":
+            print("You chose the Best First Search Algorithm")
+            print("The initial state is:")
+            print_board(initial_state)
+            print("Choose a heuristic:")
+            print("1. Misplaced Tiles")
+            print("2. Manhattan Distance")
+            print("3. Unchosen Heuristic")
+
+            bfs_choice = input("Enter your choice: ")
+
+            if bfs_choice == "1":
+                print("You chose the Misplaced Tiles heuristic")
+                solution = best_first_search(initial_state, heuristic1)
+                if solution:
+                    print(colored("Sloution found: ", "green"))
+                    print_solution(solution)
+                else:
+                    print(colored("No solution found", "red"))
+            elif bfs_choice == "2":
+                print("You chose the Manhattan Distance heuristic")
+                solution = best_first_search(initial_state, heuristic2)
+                if solution:
+                    print(colored("Sloution found: ", "green"))
+                    print_solution(solution)
+                else:
+                    print(colored("No solution found", "red"))
+            elif bfs_choice == "3":
+                print("You chose the Unchosen Heuristic")
+                solution = best_first_search(initial_state, heuristic1)
+                if solution:
+                    print(colored("Sloution found: ", "green"))
+                    print_solution(solution)
+                else:
+                    print(colored("No solution found", "red"))
+
+        elif choice == "3":
+            print("You chose to exit the program")
+            break
+
+
+
+if __name__ == "__main__":
+    main()
