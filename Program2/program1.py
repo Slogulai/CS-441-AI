9b929bb7a637b2ecb6fb91d708a3bef9bb9bae70
Slogulai <153771171+Slogulai@users.noreply.github.com>
2024-11-13 18:28:25 -0800

    Added comments

--- Program2/program1.py
+++ Program2/program1.py
@@ -8,6 +8,7 @@ POPULATION_SIZE = 100
 MUTATION_RATE = 0.1
 MAX_GENERATIONS = 1000
 
+#Is this actually the fitness??
 def fitness(member):
     # Count number of queens that are attacking each other
     attacking = 0
@@ -17,11 +18,13 @@ def fitness(member):
                 attacking += 1
     return BOARD_SIZE - attacking
 
+# I believe this to be correct for normalizing
 def normalize_fitness(population):
     fitness = [fitness(member) for member in population]
     total = sum(fitness)
     return [f / total for f in fitness] 
 
+# I believe this to be correct for select
 def select(population, normalized_fitness):
     fitness = []
     sum = 0
@@ -41,6 +44,7 @@ def select(population, normalized_fitness):
     
     return parents[0], parents[1]
 
+# I believe this to be correct for crossover
 def crossover(parent1, parent2):
     # Randomly select a crossover point
     crossover_point = random.randint(0, BOARD_SIZE - 1)
